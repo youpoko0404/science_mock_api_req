7042d8ace15a71390ed7c661b3e0869534eb8e65
Jatupat Klabklai <>
2022-06-13 12:39:29 +0700

    fix

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require("cors");
 const app = express();
-const port = 8080;
+const port = process.env.PORT || 8080;
 
 const user = require('./mock.json');
 
